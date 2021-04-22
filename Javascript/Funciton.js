028a90457b9bec23b2f8dda86744a941f9cb28d2
uneyss <73793862+uneyss@users.noreply.github.com>
2021-04-22 10:49:03 +0300

    Second install done
    Arrow function loaded

--- Javascript/Funciton.js
+++ Javascript/Funciton.js
@@ -1,5 +1,87 @@
 // Function 
 
+
+// Arrow function Start 
+
+function hello (firstName) {
+    console.log(`Merhaba ${firstName}`)
+}
+
+hello("JavaScript")
+
+const helloFuncV1 = (firstName) => {console.log(`Merhaba ${firstName}`)}
+helloFuncV1("helloFuncV1")
+
+const helloFuncV2 = firstName => console.log(`Merhaba ${firstName}`)
+helloFuncV2("helloFuncV2")
+
+
+
+const helloFuncV3 = (firstName, lastName) => 
+console.log(`Merhaba ${firstName} ${lastName}`)
+helloFuncV3("helloFuncV3","Last name info")
+
+
+
+const helloFuncV4 = (firstName, lastName) => {
+    let info = `Merhaba ${firstName} ${lastName}`
+    console.log(info)
+    return info
+}
+helloFuncV4("helloFuncV4","Other info")
+
+// Not  
+// "this" keyword;
+// this'in arrow fonksiyonlardaki davranışı, regular fonksiyonlardaki davranışından farklıdır. Nasıl ve nerede oluşturulursa oluşturulsun this'in arrow fonksiyonu içerisindeki değeri her zaman parent fonksiyonuna eşittir. 
+// Diğer bir deyişle arrow fonksiyonu kendi execution context'ini oluşturmaz. Yani kendisini referans göstermez, her zaman parent'ına bakar.
+let movie = { 
+
+    name: "La la land",
+    
+    thisInArrow:() => { 
+    console.log("Movie name is " + this.name); // 'this' window'u referans gösterir. Bu yüzden name'yi bulamaz.
+    }, 
+    
+    thisInRegular(){ 
+    console.log("Movie name is " + this.name); // 'this' kendisini referans gösterir ve çalışır.
+    } 
+    
+    };
+    movie.thisInArrow(); // output : Movie name is
+    movie.thisInRegular(); // output : Movie name is La la land
+
+const seriesList = list => {
+  
+        list.forEach((series, index) => {
+          console.log(`${index+1}. ${series}`) 
+        });
+      };
+      
+      seriesList(["Firefly", "The Mandalorian","Breaking Bad"]);
+      /* output:
+      1. Firefly
+      2. The Mandalorian
+      3. Breaking Bad
+      */
+      const newArray = (nums) => {
+
+        const newNums = nums.map(e=>{      
+            if(e%2==0){
+                return e*2
+            }else if(e%2==1){
+                return e*3
+            }
+        });
+    
+       return newNums 
+    }
+    
+    console.log(newArray([1,2,3,4,5]));  // output: [3,4,9,8,15]
+
+// Arrow function End 
+
+
+
 // Bölüm sonu egzersizi 
 let greeting = document.querySelector("#greeting")
 greeting.addEventListener("click",domClik)
