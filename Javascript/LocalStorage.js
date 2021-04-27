8d6b61739637928e8739db74f0fa3dd0fcc2a143
uneyss <73793862+uneyss@users.noreply.github.com>
2021-04-27 10:57:06 +0300

    Update

--- Javascript/LocalStorage.js
+++ Javascript/LocalStorage.js
@@ -1,4 +1,4 @@
-// Local Storage
+ // Local Storage
 
 /* 
 
--- Javascript/Object.js
+++ Javascript/Object.js
@@ -69,3 +69,56 @@ let user1 = {
 }
 
 console.log(user1.shortName())
+
+
+
+// Object Ve Array Destructing
+
+// Object Destructring 
+
+let settings = {
+    userName: "LoremIpsum",
+    password: "BadPassword",
+    isActive: false,
+    ip:"120.000.000",
+    serverName: "uneysalkis.com"
+}
+
+// Obje İçindeki Bilgilerin Tek Seferde çıkarılması 
+
+let userName = settings.userName
+console.log(userName)
+
+let {userName, password, isActive, ip, serverName} = settings
+console.log(userName, password, isActive, ip, serverName)
+
+let {userName: user, password, isActive, ip: serverIP, serverName} = settings // AS gibi almak rename && destructring
+
+
+// Obje İçindeki Bazı Bilgilerin  çıkarılması
+let {userName: userName2, password: password2, isActive: isActive2, ...newSettings} = settings  // ...newSettings kalanını buraya at demek
+
+// Objenin Destructing İle Kopyalanması
+
+/* 
+Hatalı
+let settings2 = settings
+settings2.userName = "Değişen Bilgi"
+console.log(settings)  
+console.log(settings2)   
+*/
+
+// Doğru
+let settings2 = {...settings} // Şeklini çıkart boz settings2'ye ekle
+settings2.userName = "Değişen bilgi"
+console.log(settings)  
+console.log(settings2)
+
+let score = [100, 200, 300, 400]
+
+let [score1, score2, ...otherScore] = score
+console.log(score1, score2, otherScore)
+
+// Object Kopyalama İle Aynı... let settings2 = {...settings}
+let copyOfScore = [...score]
+console.log(copyOfScore)
