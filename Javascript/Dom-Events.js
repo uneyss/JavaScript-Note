67b928441f5c839b0631d99fdbadda746025065e
uneyss <73793862+uneyss@users.noreply.github.com>
2021-06-12 18:10:04 +0300

    Update Dom-Events.js

--- Javascript/Dom-Events.js
+++ Javascript/Dom-Events.js
@@ -78,3 +78,83 @@ function run (e) {
     // console.log(e.which)
     console.log(e.key)
 } */
+
+
+
+
+
+
+
+
+
+// Mouse Eventleri 
+const cardMain = document.querySelectorAll(".card-body")[1]
+const title = document.querySelector("#tasks-title")
+
+// Click Event 
+title.addEventListener("click",run)
+
+
+// Double Click Evnet 
+title.addEventListener("dbclick",run)
+
+
+// Mouse Down
+title.addEventListener("mousedown",run)
+
+// Mouse Up
+title.addEventListener("mouseup",run)
+
+// Mouse Over Elementin üzerine gelmemiz yeterli
+title.addEventListener("mouseover",run)
+
+// Mouse Out 
+title.addEventListener("mouseout",run) 
+cardMain.addEventListener("mouseover",run)
+
+// Mouse Enter - Mouse Leave
+
+cardMain.addEventListener("mouseenter",run)
+
+
+function run(e){
+console.log(e.type)
+}
+
+
+
+
+
+
+
+// Input Eventleri 
+
+const filter = document.getElementById("filter")
+
+// Dom Loaded
+document.addEventListener("DOMContentLoaded",load)
+
+function load(e){
+    console.log('Sayfa Yüklendi')
+}
+
+// Focus 
+filter.addEventListener("focus",run)
+// Blur
+filter.addEventListener("blur",run)
+// Paste
+filter.addEventListener("paste")
+// Copy
+filter.addEventListener("copy",run)
+// Cut
+filter.addEventListener("cut",run)
+// Select 
+filter.addEventListener("cut",run)
+
+function run(e){
+    console.log(e.type)
+}
+
+
+
+// Event Capturing - Event Bubbling 
